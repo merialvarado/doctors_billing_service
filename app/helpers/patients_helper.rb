bc0927d817e665fcbfd36989f58499e9bdcd7c31
Marielyn Alvarado <marielyn.alvarado03@gmail.com>
2019-06-12 16:00:10 +0800

    Patient Mgmt: Make patient show page as modal.

--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -1,13 +1,13 @@
 module PatientsHelper
 	def payment_status_viewer(payment_status)
 		if (payment_status.upcase == "CHECK WAITING" || payment_status.upcase == "CHECK WAITING FOR FULL PAYMENT" rescue false )
-			content_tag( :a, payment_status, class: "label label-warning")
+			content_tag( :a, payment_status.upcase, class: "label label-warning")
 		elsif (payment_status.upcase == "CHECK AVAILABLE" rescue false)
-			content_tag( :a, payment_status, class: "label label-info")
+			content_tag( :a, payment_status.upcase, class: "label label-info")
 		elsif (payment_status.upcase == "FULLY PAID" rescue false)
-			content_tag( :a, payment_status, class: "label label-success")
+			content_tag( :a, payment_status.upcase, class: "label label-success")
 		else
-			content_tag( :a, payment_status, class: "label label-default")
+			content_tag( :a, (payment_status.upcase rescue nil), class: "label label-default")
 		end
 	end
 end

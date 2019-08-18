0422ad9f688da672a0e2c1b24558898b7f9c49cf
Marielyn Alvarado <marielyn.alvarado03@gmail.com>
2019-08-19 03:24:42 +0800

    Encoder: Update email to send email to Ms Joy.

--- app/jobs/send_email_job.rb
+++ app/jobs/send_email_job.rb
@@ -5,7 +5,11 @@ class SendEmailJob < ApplicationJob
     User.encoders.each do |user|
       Delayed::Worker.logger.info "Sending email to #{user.email}...."
       Rails.logger.info "Sending email to #{user.email}...."
-      PatientMailer.delay.new_patient(user, patient)
+      PatientMailer.delay.new_patient(user, patient, user.email)
+    end
+
+    [ENV['bcc_email_1'], ENV['bcc_email_2']].each do |email|
+      PatientMailer.delay.new_patient(nil, patient, email)
     end
   end
 end
--- app/mailers/patient_mailer.rb
+++ app/mailers/patient_mailer.rb
@@ -2,14 +2,9 @@ class PatientMailer < ApplicationMailer
   default from: "info.anesbillingservice@gmail.com"
   # password: @n*7b&zwN4Nz!p3$
 
-  def new_patient(user, patient)
+  def new_patient(user, patient, email)
     @patient_url = edit_patient_url(patient)
     @user = user
-    bcc_emails = []
-    if Rails.env.development?
-      bcc_emails << ENV['bcc_email_1'] 
-      bcc_emails << ENV['bcc_email_2'] 
-    end
-    mail(to: @user.email, bcc: bcc_emails, subject: '[Billing Service] New Patient for Encoding')
+    mail(to: email, subject: '[Billing Service] New Patient for Encoding')
   end
 end
